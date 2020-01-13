5731611550ef0af74187dc4b827fc9494525c9d3
satej soman <satej.soman@gmail.com>
2020-01-13 01:02:15 -0600

    Create filecounts.sh

---
+++ counts/filecounts.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+/project2/bettencourt/mnp/prclz/data/complexity
+
+
+# https://unix.stackexchange.com/a/260636
+total=0
+files=0
+find . -type f -name "*.csv" | while read FILE; do 
+  count=$(grep -c ^ < "$FILE")
+  let total=total+count
+  let files=files+1
+  echo $files
+done 
+net_blocks=$((total-files)) # adjust for headers
+echo "num GADMs : ${files}"
+echo "num blocks: ${net_blocks}"
