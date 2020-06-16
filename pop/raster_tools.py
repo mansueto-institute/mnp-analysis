e3099bd242fcc6eab4617142962ad4825bcc217f
Cooper Nederhood <cnederhood@cs.uchicago.edu>
2020-06-16 18:56:30 -0500

    Facebook pop analysis and preliminary population disaggregations

--- pop/raster_tools.py
+++ pop/raster_tools.py
@@ -6,10 +6,14 @@ import geopandas as gpd
 import pandas as pd 
 from shapely.geometry import MultiPolygon, Polygon, MultiLineString, LineString
 from rasterio.crs import CRS 
+from rasterio import features 
 from shapely.wkt import loads
 import geopandas as gdf 
 from typing import Union, List
 import affine 
+import numpy.ma as ma 
+from tobler import area_weighted 
+from tobler.area_weighted import area_tables, area_interpolate
 
 # Roots
 _ROOT = Path(__file__).resolve().parent.parent
@@ -20,80 +24,157 @@ ShapelyGeom = Union[MultiPolygon, Polygon, MultiLineString, LineString]
 
 # Temporary paths
 ls_path = Path("/home/cooper/Documents/chicago_urban/mnp/prclz-proto/data/LandScan_Global_2018/raw_tif/ls_2018.tif")
+fb_path = Path("/home/cooper/Documents/chicago_urban/mnp/prclz-proto/data/Facebook_pop/population_sle/population_sle_2019-07-01.tif")
 
 freetown_data = DATA / "Freetown"
 
 blocks_path = freetown_data / "freetown_blocks_SLE.4.2.1_1.geojson"
-blocks = gdf.read_file(freetown_data / "freetown_blocks_SLE.4.2.1_1.geojson")
+#blocks = gdf.read_file(freetown_data / "freetown_blocks_SLE.4.2.1_1.geojson")
 #
 
 def load_raster_selection(raster_io: rasterio.io.DatasetReader,
-	                      geom_list: List[ShapelyGeom]) -> np.ndarray:
-	'''
-	rasterio allows loading of subselection of a tiff file, so 
-	given a list of geometries and an open raster DatasetReader,
-	loads in the values within the geom_list.
-	This allows for loading of small selections from otherwise 
-		huge tiff's
-	'''
+                          geom_list: List[ShapelyGeom]) -> np.ndarray:
+    '''
+    rasterio allows loading of subselection of a tiff file, so 
+    given a list of geometries and an open raster DatasetReader,
+    loads in the values within the geom_list.
+    This allows for loading of small selections from otherwise 
+        huge tiff's
+    '''
 
-	if not isinstance(geom_list, List):
-		geom_list = [geom_list]
+    if not isinstance(geom_list, List):
+        geom_list = [geom_list]
 
-	# Find the window around the geom_list
-	window = rasterio.features.geometry_window(raster_io, geom_list)
-	transform = raster_io.window_transform(window)
+    # Find the window around the geom_list
+    window = rasterio.features.geometry_window(raster_io, geom_list)
+    transform = raster_io.window_transform(window)
 
-	# Perform the windowed read
-	sub_data = raster_io.read(window=window)
-	return sub_data, transform
+    # Perform the windowed read
+    sub_data = raster_io.read(window=window)
+    return sub_data, transform
 
 def save_np_as_geotiff(np_array: np.ndarray, 
-	                   transform: affine.Affine,
-	                   out_file: Path,
-	                   crs: CRS = CRS.from_epsg(4326)) -> None:
-	'''
-	Given a numpy array of data, and transform and crs defining
-	the coordinate system, save as a geotiff
-	'''
+                       transform: affine.Affine,
+                       out_file: Path,
+                       crs: CRS = CRS.from_epsg(4326)) -> None:
+    '''
+    Given a numpy array of data, and transform and crs defining
+    the coordinate system, save as a geotiff
+    '''
 
-	out_file = Path(out_file)
-	out_file.parent.mkdir(parents=True, exist_ok=True)
+    out_file = Path(out_file)
+    out_file.parent.mkdir(parents=True, exist_ok=True)
 
-	c, h, w = np_array.shape
+    c, h, w = np_array.shape
 
-	new_file = rasterio.open(
-		out_file,
-		'w',
-		driver='GTiff',
-		height=h,
-		width=w,
-		count=c,
-		dtype=np_array.dtype,
-		crs=crs,
-		transform=transform)
+    new_file = rasterio.open(
+        out_file,
+        'w',
+        driver='GTiff',
+        height=h,
+        width=w,
+        count=c,
+        dtype=np_array.dtype,
+        crs=crs,
+        transform=transform)
 
-	print("shape = {}".format(np_array.shape))
-	new_file.write(np_array)
-	# for i in range(c):
-	# 	new_file.write(np_array, c)
-	print("Saved GeoTiff at: {}".format(out_file))
+    #print("shape = {}".format(np_array.shape))
+    new_file.write(np_array)
+    print("Saved GeoTiff at: {}".format(out_file))
+
+def raster_to_geodataframe(raster_data: np.ndarray, 
+                           transform: affine.Affine) -> gpd.GeoDataFrame:
+    '''
+    Given an array of raster data and the transform, creates the correponsding
+    vector reprseentation of the data
+    ''' 
+
+    geom_types = {
+        'Polygon': Polygon
+    }
+    geom_type_multi = {
+        'Polygon': MultiPolygon
+    }
+
+    if raster_data.dtype == np.float64:
+        raster_data = raster_data.astype(np.float32)
+
+    shape_gen = list(features.shapes(raster_data, transform=transform))
+    gdf_data = {'geometry':[], 'data':[]}
+    for geom_dict, val in shape_gen:
+        geom_type = geom_types[geom_dict['type']]
+        multi_type = geom_type_multi[geom_dict['type']]
+
+        coords = geom_dict['coordinates']
+        if len(coords) <= 1:
+            geom = geom_type(*coords)
+        else:
+            # This can be done better
+            geom = multi_type([geom_type(coords[i]) for i in range(len(coords))])
+        
+        gdf_data['geometry'].append(geom)
+        gdf_data['data'].append(val)
+    
+    return gpd.GeoDataFrame(gdf_data)
+
+def extract_aoi_data_from_raster(geometry_path: str, raster_path: str, 
+                            out_path:str, save_geojson=True, save_tif=True) -> None:
+    '''
+    Extracts the relevant data from a larger raster tiff, per the geometries
+    in geometry_path file and saves out.
+
+    Inputs:
+        - geometry_path (str) geojson file of vector geometries
+        - raster_path (str) tiff file of raster data
+        - out_path (str) path to save extracted data
+    '''
+    tif_path = Path(out_path)
+    geojson_path = tif_path.with_suffix('.geojson')
+
+    raster_io = rasterio.open(raster_path)
+    freetown_geoms = gpd.read_file(geometry_path)
+
+    raster_data_selection, transform = load_raster_selection(raster_io, 
+                                                    freetown_geoms['geometry'])
+    
+    if save_tif:
+        save_np_as_geotiff(raster_data_selection, transform, str(tif_path))
+    if save_geojson:
+        gdf_data = raster_to_geodataframe(raster_data_selection, transform)
+        gdf_data.to_file(geojson_path, driver='GeoJSON')
+        print("Saved GeoJSON at: {}".format(geojson_path))
+
+def fix_invalid_polygons(geom: Union[Polygon, MultiPolygon]) -> Polygon:
+    '''
+    Fix self-intersection polygons
+    '''
+    if geom.is_valid:
+        return geom 
+    else:
+        return geom.buffer(0)
+
+freetown_ls = Path("../data/Freetown/Freetown_landscan.tif")
+freetown_fb = Path("../data/Freetown/Freetown_facebook.tif")
+
+# extract_aoi_data_from_raster(blocks_path, ls_path, freetown_ls)
+# extract_aoi_data_from_raster(blocks_path, fb_path, freetown_fb)
 
 
-def gen_freetown_LandScan(blocks_path: str, landscan_path: str) -> None:
-	'''
-	Save out the raster tiff sub-selection that is
-	the Freetown population
-	'''
+blocks = gdf.read_file(blocks_path)
 
-	raster_io = rasterio.open(landscan_path)
-	freetown_geoms = gpd.read_file(blocks_path)
+# (1) Landscan and Facebook pop apply to blocks, via block area
+pop_ls = gpd.read_file(freetown_ls.with_suffix('.geojson'))
+gt0 = pop_ls['data'] > 1
+pop_ls['data'] = pop_ls['data'] * gt0
+ls_blocks_est = area_interpolate(pop_ls, blocks, extensive_variables=['data'])
 
-	freetown_pop, transform = load_raster_selection(raster_io, 
-		                                            freetown_geoms['geometry'])
-	out_file = "../data/Freetown/Freetown_landscan.tiff"
-	save_np_as_geotiff(freetown_pop, transform, out_file)
+pop_fb = gpd.read_file(freetown_fb.with_suffix('.geojson'))
+pop_fb['geometry'] = pop_fb['geometry'].apply(fix_invalid_polygons)
+fb_blocks_est = area_interpolate(pop_fb, blocks, extensive_variables=['data'])
 
-gen_freetown_LandScan(blocks_path, ls_path)
+
+
+
+
 
 
